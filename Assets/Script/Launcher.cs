dbfa1f935a0ab3e910f569ab258f4513a150b79e
Yusril Chalif <yusrilchalif@gmail.com>
2019-10-30 19:48:50 +0700

    client server

---
+++ Assets/Script/Launcher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public GameObject connectedScreen;
+    public GameObject disconnectedScreen;
+
+    public void OnClick_Connected(){
+        PhotonNetwork.ConnectUsingSettings();
+        //Debug.Log(ConnectedScreen);
+        //PhotonNetwork.JoinLobby(TypedLobby default);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //connectedScreen.SetActive(true);
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        disconnectedScreen.SetActive(true);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        if (disconnectedScreen.activeSelf)
+        {
+
+            disconnectedScreen.SetActive(false);
+        }
+        connectedScreen.SetActive(true);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+        Debug.Log("keluar");
+    }
+}
---
+++ Assets/Script/UIHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class UIHandler : MonoBehaviourPunCallbacks
+{
+    public InputField createRoomIF;
+    public InputField joinRoomIF;
+
+    public void OnClick_CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createRoomIF.text, new RoomOptions { MaxPlayers = 2 }, null);
+    }
+
+    public void OnClick_JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinRoomIF.text, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("Room Joined Success");
+        PhotonNetwork.LoadLevel("Gameplay");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        print("Room Failed " + returnCode + "Message " + message);
+
+    }
+}
